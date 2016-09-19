a58c6a9d5ad5a92f93ffdeabfce0cee59fb2c061
pensart <guy.pensart@hotmail.com>
2016-09-19 22:56:15 +0200

    base functions for the validation

--- FormValidatorClass.php
+++ FormValidatorClass.php
@@ -1,19 +1,19 @@
 <?php
 
 /**-------------------------------------------|
-Created: 19/09/2016
-Author: Guy Pensart
-URL: http://guypensart.be
+    Created: 19/09/2016
+    Author: Guy Pensart
+    URL: http://guypensart.be
 |-------------------------------------------**/
 
 /**
- * Helper class split all inputs to objects
+ * Class helper to split all inputs into objects
  */
 class splitToObject
 {
     /**
      * splitToObject constructor.
-     * @param $value
+     * @param array
      */
     function __construct($value)
     {
@@ -23,22 +23,137 @@ class splitToObject
     }
 }
 
-
+/**
+ * Class FormValidatorClass
+ */
 class FormValidatorClass
 {
     public
     $value,
     $error,
-    $valid;
+    $valid,
+    $nextRule = TRUE,
+    $errorsFree = TRUE,
+    $input,
+    $currentInput;
 
     /**
-     * FormValdatorClass constructor.
-     * @param $post
+     * FormValidatorClass constructor.
+     * @param array $post
      */
     function __construct($post)
     {
         foreach ($post as $key => $value):
             $this->input[$key] = new splitToObject(trim($value));
         endforeach;
+    }
+
+    /**
+     * Output the value of...
+     * @param string $name
+     * @return string value
+     */
+    function getValue($name)
+    {
+        if (isset($this->input[$name]))
+            return $this->input[$name]->value;
+        return '';
+    }
+
+    /**
+     * Switch to validate object
+     * @return $this
+     */
+    function setValid()
+    {
+        if ($this->nextRule)
+            $this->currentInput->valid = TRUE;
+        return $this;
+    }
+
+    /**
+     * Output true if the input is validated
+     * @param string $name
+     * @return bool valid
+     */
+    function getValid($name)
+    {
+        if (isset($this->input[$name]))
+            return $this->input[$name]->valid;
+        return '';
+    }
+
+    /**
+     * Check if there are no errors
+     * @return bool
+     */
+    function errorsFree()
+    {
+        return $this->errorsFree;
+    }
+
+    /**
+     * Clear all inputs
+     */
+    function clearFields()
+    {
+        if ($this->errorsfree())
+            foreach($this->input as $key=>$value):
+                $this->input[$key]->value = '';
+            endforeach;
+    }
+
+    /**
+     * Temporary debugging info function
+     * @return string $debug
+     */
+    function debug()
+    {
+        $debug = '';
+        foreach($this->input as $key=>$value):
+            $debug .= '<span class="debug-';
+            $debug .= ($this->getValid($key)) ? 'passed' : 'error';
+            $debug .= '">';
+            $debug .= $key;
+            $debug .= '</span>';
+        endforeach;
+        $debug .= '<span class="debug-';
+        $debug .= ($this->errorsFree()) ? 'passed' : 'error';
+        $debug .= '">';
+        $debug .= 'Completed';
+        $debug .= '</span>';
+        return $debug;
+    }
+
+    //-----------------------------------------------------------
+    // Validation rules, always start validating with input($name)
+    //-----------------------------------------------------------
+
+    /**
+     * Set currentInput before other validations
+     * @param string $name
+     * @return $this
+     */
+    function item($name)
+    {
+        if (!isset($this->input[$name]))
+            $this->input[$name] = new splitToObject('');
+        $this->nextRule = TRUE;
+        $this->currentInput = $this->input[$name];
+
+        return $this;
+    }
+
+    /**
+     * Field is required
+     * @return $this
+     */
+    function required()
+    {
+        if ($this->nextRule)
+        {
+            $this->nextRule = ( $this->currentInput->value != '') ? TRUE : FALSE;
+        }
+        return $this;
     }
 }
