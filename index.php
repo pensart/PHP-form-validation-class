6ad8404ed21d52c27c454f97cfc8be64d407d508
pensart <guy.pensart@hotmail.com>
2016-09-19 18:46:42 +0200

    Start project

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+/**-------------------------------------------|
+    Created: 19/09/2016
+    Author: Guy Pensart
+    URL: http://guypensart.be
+|-------------------------------------------**/
+
+$placeholder =
+    [
+        'name'=>'Placeholder name',
+        'email'=>'Placeholder email',
+        'message'=>'Placeholder message'
+    ];
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
+    <title>PHP Form validator class</title>
+    <style>
+        html { font-family: sans-serif; }
+        h1 { text-align: center; }
+        form { min-width: 500px; }
+        input, textarea { padding: .5em; width: 100%; margin-bottom: 20px; }
+        textarea { height: 11em; }
+        .wrapper { display: table; margin: 0 auto; display: table; }
+    </style>
+</head>
+    <body>
+    <h1>'Contact me'</h1>
+    <div class="wrapper">
+        <form method="POST">
+            <input type="text" name="name" placeholder=<?= '"'.$placeholder['name'].'"';?> >
+            <input type="text" name="email" placeholder=<?= '"'.$placeholder['email'].'"';?> >
+            <textarea name="message"><?= $placeholder['message'] ?></textarea>
+            <input type="submit" >
+        </form>
+    </div>
+    </body>
+</html>
---
+++ readme.md
@@ -0,0 +1,20 @@
+# PHP form validation class
+### Features
+* Simple to setup
+* validate in sequence or linear
+* Extensible for your own validations
+
+### Work in progress
+- [x] Basic form to work with
+- [ ] Write class and helper class
+- [ ] Define vars
+- [ ] Getters and Setters for the values
+- [ ] Error check and reporting
+- [ ] Check required
+- [ ] Check min and max number of characters
+- [ ] Check if string has letters
+- [ ] Check if email looks valid
+- [ ] convert email to lowercase characters
+- [ ] Sanitize email
+- [ ] Filter the given email
+- [ ] Check text-area and remove special characters
